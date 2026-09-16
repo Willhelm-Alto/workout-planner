a754931b6642cd54e6fc3be1cdaa323c9151856f
Willhelm-Alto <souzaguilherme1234567890@gmail.com>
2026-09-16 15:44:16 -0300

    visual: weight card & fix chips alignment

--- lib/pages/execution_page.dart
+++ lib/pages/execution_page.dart
@@ -21,6 +21,7 @@ class _ExecutionPageState extends State<ExecutionPage> {
   int _secondsLeft = 0;
   int currentSet = 1;
   bool workoutStarted = false;
+  final weightController = TextEditingController();
 
   String get stopwatchStr {
     final e = stopwatch.elapsed;
@@ -41,6 +42,9 @@ class _ExecutionPageState extends State<ExecutionPage> {
       doneExercisesList[e] = false;
     }
     current = doneExercisesList.entries.firstWhere((e) => !e.value).key;
+    current.weight != null
+        ? weightController.text = current.weight.toString()
+        : weightController.text = '-';
   }
 
   @override
@@ -143,124 +147,185 @@ class _ExecutionPageState extends State<ExecutionPage> {
           ],
         ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-            child: Column(
-              spacing: 12,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  current.title.toUpperCase(),
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Chip(
-                      backgroundColor: Colors.blue.shade50,
-                      side: BorderSide.none,
-                      visualDensity: VisualDensity.compact,
-                      labelStyle: TextStyle(),
-                      label: Text('${current.repetitions} reps'),
-                    ),
-                    Chip(
-                      backgroundColor: Colors.blue.shade50,
-                      side: BorderSide.none,
-                      visualDensity: VisualDensity.compact,
-                      labelStyle: TextStyle(),
-                      label: Text('${current.set} set'),
-                    ),
-                    Chip(
-                      backgroundColor: Colors.blue.shade50,
-                      side: BorderSide.none,
-                      visualDensity: VisualDensity.compact,
-                      labelStyle: TextStyle(),
-                      label: Text('${current.restTime} rest'),
-                    ),
-                  ],
-                ),
-                Card(
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    side: BorderSide(color: Colors.grey.shade300),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                      vertical: 16,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      spacing: 8,
-                      children: [
-                        Text("Set ${currentSet} of ${current.set}"),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          spacing: 6,
-                          children: List.generate(current.set, (i) {
-                            final done = i < currentSet - 1;
-                            final active = i == currentSet - 1;
-                            return Container(
-                              width: 12,
-                              height: 12,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: done ? Colors.blue : Colors.transparent,
-                                border: Border.all(
-                                  width: 2,
-                                  color: done || active
-                                      ? Colors.blue
-                                      : Colors.grey.shade500,
-                                ),
-                              ),
-                            );
-                          }),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    if (_isTimer)
-                      SizedBox(
-                        width: 108,
-                        height: 108,
-                        child: CircularProgressIndicator(
-                          value: _secondsLeft / current.restTime,
-                          strokeWidth: 3,
-                          backgroundColor: Colors.blue.shade50,
-                          valueColor: AlwaysStoppedAnimation(Colors.blue),
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    spacing: 12,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        current.title.toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
-                    ElevatedButton(
-                      onPressed: _toggleTimer,
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: EdgeInsets.all(10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Chip(
+                            backgroundColor: Colors.blue.shade50,
+                            side: BorderSide.none,
+                            visualDensity: VisualDensity.compact,
+                            labelStyle: TextStyle(),
+                            label: Text('${current.repetitions} reps'),
+                          ),
+                          Chip(
+                            backgroundColor: Colors.blue.shade50,
+                            side: BorderSide.none,
+                            visualDensity: VisualDensity.compact,
+                            labelStyle: TextStyle(),
+                            label: Text('${current.set} set'),
+                          ),
+                          Chip(
+                            backgroundColor: Colors.blue.shade50,
+                            side: BorderSide.none,
+                            visualDensity: VisualDensity.compact,
+                            labelStyle: TextStyle(),
+                            label: Text('${current.restTime} rest'),
+                          ),
+                        ],
+                      ),
+                      Card(
                         elevation: 0,
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        fixedSize: const Size(92, 92),
-                      ),
-                      child: !workoutStarted
-                          ? Text("START")
-                          : _isTimer
-                          ? Text(
-                              "$_secondsLeft",
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 16,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Set ${currentSet} of ${current.set}"),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                spacing: 6,
+                                children: List.generate(current.set, (i) {
+                                  final done = i < currentSet - 1;
+                                  final active = i == currentSet - 1;
+                                  return Container(
+                                    width: 12,
+                                    height: 12,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: done
+                                          ? Colors.blue
+                                          : Colors.transparent,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: done || active
+                                            ? Colors.blue
+                                            : Colors.grey.shade500,
+                                      ),
+                                    ),
+                                  );
+                                }),
                               ),
-                            )
-                          : const Icon(Icons.check),
-                    ),
-                  ],
+                            ],
+                          ),
+                        ),
+                      ),
+                      Card(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 16,
+                          ),
+                          child: Column(
+                            spacing: 12,
+                            children: [
+                              Row(
+                                spacing: 6,
+                                children: [
+                                  Icon(Icons.fitness_center, size: 16),
+                                  Text("Weight"),
+                                ],
+                              ),
+                              Row(
+                                spacing: 12,
+                                children: [
+                                  Expanded(
+                                    child: TextField(
+                                      controller: weightController,
+                                      decoration: InputDecoration(
+                                        contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+                                        suffixText: "kg",
+                                      ),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Colors.blue,
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
+                                      padding: const EdgeInsets.symmetric(vertical: 14)
+                                    ),
+                                    child: Text(
+                                      "Save",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          if (_isTimer)
+                            SizedBox(
+                              width: 108,
+                              height: 108,
+                              child: CircularProgressIndicator(
+                                value: _secondsLeft / current.restTime,
+                                strokeWidth: 3,
+                                backgroundColor: Colors.blue.shade50,
+                                valueColor: AlwaysStoppedAnimation(Colors.blue),
+                              ),
+                            ),
+                          ElevatedButton(
+                            onPressed: _toggleTimer,
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: EdgeInsets.all(10),
+                              elevation: 0,
+                              backgroundColor: Colors.blue,
+                              foregroundColor: Colors.white,
+                              fixedSize: const Size(92, 92),
+                            ),
+                            child: !workoutStarted
+                                ? Text("START")
+                                : _isTimer
+                                ? Text(
+                                    "$_secondsLeft",
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  )
+                                : const Icon(Icons.check),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
